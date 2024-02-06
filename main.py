b49a16c6b797f12c589671c838f7878325ff9edd
Vlad <hennessyfgh@gmail.com>
2024-02-06 21:12:24 +0300

    for reading json commit

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main():
     sort_items = sort_date(sorted_date)
     items = chosen_actions(sort_items)
     for item in items:
-    #     print(date_correction(item['date']), item['description'])
-    #     print(get_card(item), get_account(item))
+          print(date_correction(item['date']), item['description'])
+          print(get_card(item), get_account(item))
           print(get_money(item))
           print()
 
--- read_json.py
+++ read_json.py
@@ -4,6 +4,6 @@ import os
 
 def load_json():
     """Чтение json-файла"""
-    with open(os.path.join('..', 'cours_work_backend', 'operations.json'), 'r', encoding="utf-8") as read_file:
+    with open(os.path.join('operations.json'), 'r', encoding = "utf-8") as read_file:
         data = json.load(read_file)
         return data
